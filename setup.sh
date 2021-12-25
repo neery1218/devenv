6f41e528f410af8ee42021b6bf22c15d84982387
neerajensritharan <neery1218@gmail.com>
2021-12-24 17:54:54 -0700

    setup

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+if [[ "$(uname)" == "Linux" ]]; then
+	sudo apt install neovim fzf ag
+fi
+
+# my config files
+cp .tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.config/nvim
+cp -r nvim/* ~/.config/nvim/
