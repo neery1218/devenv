20610b97d5b47c3ee587f2fb33d69abb542f2dc4
neery1218 <neerajen.sritharan@gmail.com>
2022-01-21 15:58:51 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,7 +51,7 @@ source rust-install.sh -y
 
 # install golang
 wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 pip3 install --upgrade pip
