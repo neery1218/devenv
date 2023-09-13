dc5229a5324692d19d0df8ff0f30469da2fe3a62
neery1218 <neery1218@gmail.com>
2023-09-13 09:30:50 +0800

    test

--- setup.sh
+++ setup.sh
@@ -39,7 +39,14 @@ fi
 echo "Installing node..."
 # installing from source takes too long (especially on aws micro instances) so lets do this instead
 # curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 # my config files
 cp .tmux.conf ~/.tmux.conf
